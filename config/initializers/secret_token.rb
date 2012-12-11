e88b1e3785d14e64b0473c1684717f75945f8235
jodyalbritton <jody.albritton@gmail.com>
2012-12-11 00:55:09 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opero::Application.config.secret_token = '478520549cc255c7fecf7ef69de11242d491cd9ae65a11f2d99836fbbc53f2e2ecb260714681e5683f63d9a0abe658e7e5596b579e781a9d057eb5eaaf96387b'
---
+++ config/opero_unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/var/src/opero"
+pid "/var/src/getoptimus/tmp/pids/unicorn.pid"
+stderr_path "/var/src/opero/log/unicorn.log"
+stdout_path "/var/src/opero/log/unicorn.log"
+
+listen "/tmp/unicorn.opero.sock", :backlog => 64
+worker_processes 2
+timeout 30
